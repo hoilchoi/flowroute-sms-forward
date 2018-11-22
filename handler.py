614160b7db6219b04c1cd56f6ba243a1b3386c61
Hoil Choi <hchoi@flowroute.com>
2018-11-21 23:57:33 -0800

    initial test works, but duplicated for some reason

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ import json
 import boto3
 from botocore.vendored import requests
 import config
+import time
 
 
 MY_DYNAMODB = boto3.resource('dynamodb', region_name='us-west-2')
@@ -20,6 +21,7 @@ def make_response(code, message, detail=None):
 
 def log_inbound_sms(event, forward=True):
     """Log inbound SMS."""
+    print("INFO: log inbound sms start")
     mdr = event['data']['id']
     from_number = event['data']['attributes']['from']
     to_number = event['data']['attributes']['to']
@@ -44,30 +46,34 @@ def log_inbound_sms(event, forward=True):
 
     # forward message
     if forward:
-        # print("forwarding")
-        send_url = config.sms_url
+        print("INFO: forwarding sms start")
+        send_url = config.SMS_URL
         headers = {'content-type': 'application/json'}
-        auth = (config.api_key, config.api_secret)
-        message_body = ("""FORWARDED MESSAGE\n
-                        Original Sender: {}\n
-                        Original Receiver: {}\n
+        auth = (config.API_KEY, config.API_SECRET)
+        message_body = ("""FORWARDED MESSAGE
+                        Original Sender: {}
+                        Original Receiver: {}
                         {}"""
                         .format(from_number, to_number, message_body))
         to_number = "12532735442"
         body = {"to": to_number,
-                "from": config.from_number,
+                "from": config.FROM_NUMBER,
                 "body": message_body,
                 }
 
         # sent outbound sms
-        requests.post(url=send_url, auth=auth, json=body,
+        forward_message = requests.post(url=send_url, auth=auth, json=body,
                       headers=headers)
+        time.sleep(3)
+        forward_response = forward_message.json()
+        log_outbound_sms(forward_response)
 
     return make_response(200, "SMS saved to Dynamo", detail=log_item)
 
 
 def log_outbound_dlr(event):
     """Log outbound sms dlr."""
+    print("INFO: log forwarded dlr start")
     mdr = event['data']['id']
     from_number = event['data']['attributes']['from']
     to_number = event['data']['attributes']['to']
@@ -93,12 +99,13 @@ def log_outbound_dlr(event):
 
 def log_outbound_sms(event):
     """Log outbound sms result."""
+    print("INFO: log outbound sms start")
     mdr = event['data']['id']
 
     # update outbound to dynamo
     log_item = {
         'id': mdr,
-        'from': config.from_number,
+        'from': config.FROM_NUMBER,
         'direction': "forward",
         # make it so that orig sender, orig to becomes column in db
         # then rename outbound-sms to forward-sms
@@ -112,17 +119,18 @@ def log_outbound_sms(event):
     return make_response(200, "SMS saved to Dynamo", detail=log_item)
 
 
-def receive_inbound_sms(event):
+def receive_inbound_sms(event, context):
     """Handle incoming events."""
     print("called")
     print(event)
     event_body = json.loads(event['body'])
+    #event_body = event['body']
     # sms = event
 
-    if event_body['data']['attributes']['delivery_receipts']:
-        response = log_outbound_dlr(event_body)
-    elif event_body['data']['attributes']['direction'] == "inbound":
-        response = log_inbound_sms(event_body, forward=True)
-    else:
-        response = log_outbound_sms(event_body)
+    #if 'delivery_receipts' in event_body['data']['attributes']:
+    #    response = log_outbound_dlr(event_body)
+    #elif event_body['data']['attributes']['direction'] == "inbound":
+    response = log_inbound_sms(event_body, forward=True)
+    #else:
+    #    response = log_outbound_sms(event_body)
     return response
